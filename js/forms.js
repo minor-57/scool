24e7a7683278d09e5b134cb2b114c25d0832582b
minor-57 <agapovdanya@yandex.ru>
2023-09-08 12:32:06 +0300

    form sending functional added

--- js/forms.js
+++ js/forms.js
@@ -1,72 +1,21 @@
-"use strict"
+const form = document.querySelector(".call-back__form");
+form.addEventListener("submit", formSend);
 
-window.addEventListener('DOMContentLoaded', function(){
-    const form = document.getElementById('call-back-form');
-    form.addEventListener('submit', formSend);
+async function formSend(event) {
+    event.preventDefault();
 
-    async function formSend(e){
-        e.preventDefault();
+    const formData = new FormData(form);
 
-        let error =  formValidate(form);
-        let formData = new FormData(form);
+    let response = await fetch('php/sendmail.php', {
+        method: 'POST',
+        body: formData,
+        mode: 'no-cors'
+    });
 
-        if (error === 0){
-            let response = await fetch('sendmail.php', {
-                method: 'POST',
-                body: formData
-            });
-            if (response.ok){
-                let result = await response.json();
-                alert(result.message);
-                form.reset();
-            }else{
-
-            }
-
-        }else{
-            alert('Заполните обязательные поля!'); 
-        }
+    if(response.ok){
+        alert("mail sent");
+        form.reset();
+    } else{
+        alert("error");
     }
-
-    function formValidate(form){
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
-
-        formReq.forEach(function(el){
-            const input = el;
-            formRemoveError(input);
-
-            if(input.classList.contains('_email')){
-                if(emailTest(input)){
-                    formAddError(input);
-                    error++;
-                }
-            }else if (input.getAttribute("type") === "checkbox" && input.checked === false){
-                    formAddError(input);
-                    error++;
-                }else{
-                    if(input.value === ''){
-                        formAddError(input);
-                        error++;
-                    }
-                }
-
-            
-        })
-        return error;
-    }
-
-    function formAddError(input){
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-
-    function formRemoveError(input){
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
-
-    function emailTest(input){
-        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-    }
-})
+}
---
+++ php/sendmail.php
@@ -0,0 +1,49 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'daniilagapov822@gmail.com';                     //SMTP username
+    $mail->Password   = 'ymtsvzartztwuwci';                               //SMTP password
+    $mail->SMTPSecure = 'TLS';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('daniilagapov822@gmail.com', 'Olimpus');
+    $mail->addAddress('agapovdanya@yandex.ru', 'Test User');     //Add a recipient
+
+    //Тело письма
+    $body = '<h1> Форма:</h1>';
+    $body .= '<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+    $body .= '<p><strong>Телефон:</strong> '.$_POST['number'].'</p>';
+
+    $mail->Body = $body;
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = $body;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+    ?>
