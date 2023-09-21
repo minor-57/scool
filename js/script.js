e46d49e29f47c882de156b121d522f3afc7fac99
cxcggg <sanek44ishin@yandex.ru>
2023-09-21 03:50:08 +0300

    Сделал какую то хуйню по курсам, это только первая страница

--- js/script.js
+++ js/script.js
@@ -15,14 +15,29 @@ window.addEventListener('DOMContentLoaded', function(){
     document.querySelector('.menu-overlay').addEventListener('click', function(){
         document.querySelector('.left-menu').classList.remove('left-menu-active');
         document.querySelector('.menu-overlay').classList.remove('menu-overlay-active');
-        document.querySelector('._modal-wraper').classList.remove('active');
+
+        document.querySelectorAll('.account-tab').forEach(function(el){
+            el.classList.remove('active');
+        });
+        document.querySelector('.account__login').classList.add('active');
+
+        document.querySelectorAll('._modal-wraper').forEach(function(el){
+            el.classList.remove('active');
+        })
+        document.querySelector('body').classList.remove('no-scrol')
     });
 
     document.querySelectorAll('.left-menu__link').forEach(function(link){
+        if (!link.classList.contains('catalog-btn')){
         link.addEventListener('click', function(){
             document.querySelector('.left-menu').classList.remove('left-menu-active');
             document.querySelector('.menu-overlay').classList.remove('menu-overlay-active');
-        })
+        })}
+    });
+    document.querySelector('.left-menu__catalog-btn').addEventListener('click', function(){
+        document.querySelector('.left-menu__catalog-btn').classList.toggle('catalog-btn-active');
+        document.querySelector('.left-menu__catalog-menu').classList.toggle('catalog-left-active');
+        document.querySelector('.click-close').classList.toggle('active');
     });
     // catalog
     
@@ -33,8 +48,13 @@ window.addEventListener('DOMContentLoaded', function(){
     });
 
     document.querySelector('.click-close').addEventListener('click', function(){
-        document.querySelector('.catalog-btn').classList.remove('catalog-btn-active');
-        document.querySelector('.catalog-menu').classList.remove('catalog-menu-active');
+        document.querySelectorAll('.catalog-btn').forEach(function(el){
+            el.classList.remove('catalog-btn-active');
+        });
+        document.querySelectorAll('.catalog-menu').forEach(function(el){
+            el.classList.remove('catalog-left-active');
+            el.classList.remove('catalog-menu-active');
+        });
         document.querySelector('.click-close').classList.remove('active');
     })
 
@@ -57,25 +77,57 @@ window.addEventListener('DOMContentLoaded', function(){
 
     // section clients
 
-    document.querySelector('.clients__btn-more').addEventListener('click', function(){
+    document.querySelector('.clients__btn-more').addEventListener('click', function(el){
         document.querySelectorAll('.clients__item').forEach(function(item){
             item.style.display = 'block';
         })
+        document.querySelector('.clients__btn-more').classList.add('hidden');
     })
-    // modal window
+    // modal windows
 
     document.querySelector('.header__order-call').addEventListener('click', function(){
-        document.querySelector('._modal-wraper').classList.add('active');
-        document.querySelector('.menu-overlay').classList.add('menu-overlay-active')
+        document.querySelector('.call-back-wraper').classList.add('active');
+        document.querySelector('.menu-overlay').classList.add('menu-overlay-active');
+        document.querySelector('body').classList.add('no-scrol')
     });
     document.querySelector('.header__call-btn').addEventListener('click', function(){
-        document.querySelector('._modal-wraper').classList.add('active');
+        document.querySelector('.call-back-wraper').classList.add('active');
         document.querySelector('.menu-overlay').classList.add('menu-overlay-active')
+        document.querySelector('body').classList.add('no-scrol')
     });
-    document.querySelector('.modal-close').addEventListener('click', function(){
-        document.querySelector('._modal-wraper').classList.remove('active');
-        document.querySelector('.menu-overlay').classList.remove('menu-overlay-active');
+
+    document.querySelectorAll('.modal-close').forEach(function(el){
+        el.addEventListener('click', function(){
+            document.querySelector('body').classList.remove('no-scrol')
+            document.querySelectorAll('._modal-wraper').forEach(function(el){
+                el.classList.remove('active');
+            })
+            document.querySelector('.menu-overlay').classList.remove('menu-overlay-active');
+
+            document.querySelectorAll('.account-tab').forEach(function(el){
+                el.classList.remove('active');
+            });
+            document.querySelector('.account__login').classList.add('active');
+            document.querySelector('body').classList.remove('no-scrol')
+    })
+
     });
+    document.querySelector('.login-btn').addEventListener('click', function(){
+        document.querySelector('.account-wraper').classList.add('active');
+        document.querySelector('.menu-overlay').classList.add('menu-overlay-active');
+        document.querySelector('body').classList.add('no-scrol')
+    })
+
+    document.querySelectorAll('.account-tab-btn').forEach(function(item){
+        item.addEventListener('click', function(event){
+            const path = event.currentTarget.dataset.path_modal;
+            document.querySelectorAll('.account-tab').forEach(function(el){
+                el.classList.remove('active');
+            });
+
+            document.querySelector(`[data-target_modal="${path}"]`).classList.add('active');
+        })
+    })
     // form mask
 
     document.getElementById('phone').addEventListener('input', function (e) {
